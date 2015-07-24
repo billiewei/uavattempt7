1f3a3dd23b9b3c6e27e572d3d7293c789c212007
Billie Wei <billiewei@gmail.com>
2015-07-24 12:18:07 +0800

    Update manual control ui

--- manualcontrolhandler.cpp
+++ manualcontrolhandler.cpp
@@ -1,1 +1,11 @@
 #include "manualcontrolhandler.h"
+
+void ManualControlHandler::onSetArming(){
+
+//    if (armingstatetoggle->clicked()) {
+//         serial->set_mode_arm() }
+//    else{ serial->set_mode_disarm() }
+
+    connect(armingstatetoggle,SIGNAL(clicked()),this, SLOT(onSetArming()));
+
+}
--- manualcontrolhandler.h
+++ manualcontrolhandler.h
@@ -1,15 +1,53 @@
 #ifndef MANUALCONTROLHANDLER
 #define MANUALCONTROLHANDLER
 
+#include <QtCore/QtGlobal>
 #include <QQuickItem>
+
+#include <QMainWindow>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QGridLayout>
+#include <QGroupBox>
+#include <QComboBox>
+#include <QSlider>
+#include <QLabel>
+#include <QPushButton>
+#include <QRadioButton>
 
 class ManualControlHandler : public QQuickItem {
 
+    Q_OBJECT
+//    Q_PROPERTY(QString armingstate READ armingstate WRITE onSetArming NOTIFY armingStateChanged)
+
+//    serial = new MavSerialPort(this);
+
     public:
+        enum ARM_STATE {DISARMED = 0, ARMED = 1} arm_state;
     public slots:
     private:
+        QRadioButton* armingstatetoggle;
+
+        /** Manual control sliders */
+        int throttle_control;
+        int yaw_control;
+        int pitch_control;
+        int roll_control;
+
+        QSlider* throttleSlider;
+        QSlider* yawSlider;
+        QSlider* pitchSlider;
+        QSlider* rollSlider;
+        QLabel* throttleValue;
+        QLabel* yawValue;
+        QLabel* pitchValue;
+        QLabel* rollValue;
+
+    private slots:
+        void onSetArming();
+
     signals:
-        void armingstateChange();
+        void armingStateChanged(ARM_STATE s);
         void throttleChanged();
         void yawChanged();
         void pitchChanged();
